150004e8bc67eed8f0101882b12130b77d4ef743
scl <st.classen@gmx.ch>
2012-10-01 17:02:41 +0200

    udpated license header in all files

--- guice-jpa/src/test/java/ch/sclassen/guice/guicejpa/SingleTransactionTest.java
+++ guice-jpa/src/test/java/ch/sclassen/guice/guicejpa/SingleTransactionTest.java
@@ -1,4 +1,7 @@
 /**
+ * Copyright (C) 2012 Stephan Classen
+ * Based on guice-perist (Copyright (C) 2010 Google, Inc.)
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -11,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package ch.sclassen.guice.guicejpa;
 
 import junit.framework.TestCase;
